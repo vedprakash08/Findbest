e650379275aa0b851678ead67599806efa625f40
vedprakash08 <ved9552231@gmail.com>
2024-07-11 16:04:56 +0530

    rolback

--- Frontend/src/backend.js
+++ Frontend/src/backend.js
@@ -1,3 +1,3 @@
 // export const API = "https://findbest.vercel.app/";
-// export const API = "http://localhost:4000";
-export const API = "https://findbest-backend-0zl1.onrender.com"
+export const API = "http://localhost:4000";
+// export const API = "https://findbest-backend-0zl1.onrender.com"
