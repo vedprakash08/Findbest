bc6ab28d04a29a1cf464d0e71a69a3550e34054b
vedprakash08 <ved9552231@gmail.com>
2024-07-11 15:38:06 +0530

    D

--- Backend/Categories/ClothingWebsites/SnapdealModule.js
+++ Backend/Categories/ClothingWebsites/SnapdealModule.js
@@ -11,7 +11,9 @@ puppeteer.use(AdblockerPlugin({ blockTrackers: true }));
 const getClothesSnapdeal = async (URL) => {
   try {
     const data = [];
-    const browser = await puppeteer.launch({ headless: "new" });
+    // const browser = await puppeteer.launch({ headless: "new" });
+    const browser = await puppeteer.launch({ headless: "new", args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+
     const page = await browser.newPage();
 
     await page.goto(URL);
--- Frontend/src/backend.js
+++ Frontend/src/backend.js
@@ -1,1 +1,2 @@
 export const API = "https://findbest.vercel.app/";
+// export const API = "http://localhost:4000";
